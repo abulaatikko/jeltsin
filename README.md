b3cf0b04a5217fbd0e7652adf83163c8b895098f
Lassi Heikkinen <lassi@pulu.org>
2016-12-20 19:27:28 +0000

    Add Yle uutiset ulkomaa fetch

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # jeltsin
 Yle uutiset link crawler
+
+# TODO
+
+* init mongodb
+* insert new news items to db
+* show the 1000 newest news in web page
+* support other categories (talous)
--- crawler.js
+++ crawler.js
@@ -1,1 +1,17 @@
-console.log('Hello world')
+let request = require('request')
+let cheerio = require('cheerio')
+let moment = require('moment')
+
+const yleUlkomaatUrl = 'http://yle.fi/uutiset/18-34953'
+
+request(yleUlkomaatUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(body)
+        $('article').each((index, elem) => {
+            const newsUrl = $(elem).find('a').attr('href')
+            const title = $(elem).find('h1').text()
+            const newsAddedRaw = $(elem).find('time').attr('datetime')
+            console.log(newsUrl, title, moment(newsAddedRaw).toISOString(), moment().toISOString())
+        })
+    }
+})
