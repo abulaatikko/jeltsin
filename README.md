87ee15fc5da8c9d467a5a0b75f88ee5159f15825
Lassi Heikkinen <lassi@pulu.org>
2016-12-22 14:33:46 +0000

    Insert new news items to mongodb

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # jeltsin
 Yle uutiset link crawler
 
+# USAGE
+
+1. sudo service mongodb start
+1. node crawler.js
+1. mongo jeltsin
+
 # TODO
 
-* init mongodb
 * insert new news items to db
+    * one query to check which of the urls are not in db
+    * clean callback hell (promises?)
 * show the 1000 newest news in web page
 * support other categories (talous)
--- crawler.js
+++ crawler.js
@@ -1,17 +1,71 @@
 let request = require('request')
 let cheerio = require('cheerio')
 let moment = require('moment')
+let mongo = require('mongodb')
+
+let mongoClient = mongo.MongoClient
 
 const yleUlkomaatUrl = 'http://yle.fi/uutiset/18-34953'
+const rootUrl = 'http://yle.fi'
+const mongoUrl = 'mongodb://localhost:27017/jeltsin'
+
+let insertAllData = function(newData) {
+    mongoClient.connect(mongoUrl, function(error, db) {
+        if (error) {
+            console.log(error)
+        }
+
+        let collection = db.collection('news')
+
+        let insertData = []
+        collection.find({}).toArray(function(error, currentData) {
+            if (error) {
+                console.log(error)
+            }
+
+            newData.forEach(newRow => {
+                let found = false
+                currentData.forEach(currentRow => {
+                    if (currentRow.url === newRow.url) {
+                        found = true
+                    }
+                })
+
+                if (!found) {
+                    insertData.push(newRow)
+                }
+            })
+
+            if (insertData.length > 0) {
+                collection.insertMany(insertData, function(error, result) {
+                    if (error) {
+                        console.log(error)
+                    }
+                    db.close()
+                })
+            } else {
+                db.close()
+            }
+        })
+    })
+}
 
 request(yleUlkomaatUrl, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         const $ = cheerio.load(body)
+        let data = []
         $('article').each((index, elem) => {
             const newsUrl = $(elem).find('a').attr('href')
             const title = $(elem).find('h1').text()
             const newsAddedRaw = $(elem).find('time').attr('datetime')
-            console.log(newsUrl, title, moment(newsAddedRaw).toISOString(), moment().toISOString())
+            data.push({
+                url: rootUrl + newsUrl,
+                title: title,
+                added: moment(newsAddedRaw).toISOString(),
+                created: moment().toISOString(),
+            })
         })
+
+        insertAllData(data)
     }
 })
